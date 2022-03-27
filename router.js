3556d01713555b46932368990867cd562b9a6ec2
Leo <leosilva163@hotmail.com>
2022-03-27 12:18:49 -0300

    finished provider order controller

--- router.js
+++ router.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer({});
 const LoginController = require('./src/Controller/LoginController');
 const BalanceController = require ('./src/Controller/BalanceController');
 const ProductController = require('./src/Controller/ProductController');
@@ -22,6 +24,7 @@ router.post('/provider/create', ProviderController.create);
 router.patch('/provider/:id/update', ProviderController.update);
 router.get('/provider/list', ProviderController.list);
 
-router.post('/provider-order/create', ProviderOrderController.create);
+router.post('/provider-order/create', upload.fields([{name : 'first_payment'}, {name : 'datasheet'}]), ProviderOrderController.create);
+router.get('/provider-order/list', ProviderOrderController.list);
 
 module.exports = router;
--- src/Controller/ProviderOrderController.js
+++ src/Controller/ProviderOrderController.js
@@ -1,46 +1,50 @@
+const res = require('express/lib/response');
+const fs = require('fs');
+const moment = require('moment');
 const ProviderOrder = require('../models/ProviderOrderModel');
+
 
 class ProviderOrderController {
     async create(req, res){
         try {
-            const order = req.body;
-            console.log(req.body);
-            // const data = req.body;
-            // const productId = req.body.product_id;
-            // const providerId = req.body.provider_id;
-            // const totalValue = req.body.total_value;
-            // const totalPaid = req.body.total_paid;
-            // const userName = req.body.user_name;
-            // const firstPayment = req.body.first_payment;
-            // const firstPaymentDate = req.body.first_payment_date;
-            // const secondPayment = req.body.second_payment;
-            // const secondPaymentDate = req.body.second_payment_date;
-            // const datasheet = req.body.datasheet;
-            // const referenceProduct = req.body.reference_product;
-            // const recievedOrder = req.body.received_order;
-            // const deadline = req.body.deadline;
+            const firstPayment = req.files['first_payment'];
+            const datasheet = req.files['datasheet'];
+            const data = req.body;
 
-            // const order = await ProviderOrder.create({
-            //     "product_id" : productId,
-            //     "provider_id" : providerId,
-            //     "total_value" : totalValue,
-            //     "total_paid" : totalPaid,
-            //     "user_name" : userName,
-            //     "first_payment" : firstPayment,
-            //     "first_payment_date" : firstPaymentDate,
-            //     "second_payment" : secondPayment,
-            //     "second_payment_date" : secondPaymentDate,
-            //     "datasheet" : datasheet,
-            //     "reference_product" : referenceProduct,
-            //     "received_order" : recievedOrder,
-            //     "deadline" : deadline
-            // });
+            await fs.writeFileSync(`./uploads/${firstPayment[0].originalname}`, firstPayment[0].buffer, (err) => {
+                if (err) {
+                    throw new Error('Não foi possível cadastrar o arquivo de primeiro pagamento');
+                }
+            });
+            await fs.writeFileSync(`./uploads/${datasheet[0].originalname}`, datasheet[0].buffer), (err) => {
+                if (err) {
+                    throw new Error('Não foi possível cadastrar o arquivo da Ficha técnica');
+                }
+            };
+
+            data.first_payment_date = moment(data.first_payment_date, "YYYY-MM-DD");
+            data.second_payment_date = data.second_payment_date ? moment(data.second_payment_date, "YYYY-MM-DD") : null;
+            data.first_payment = `./uploads/${firstPayment[0].originalname}`;
+            data.second_payment = data.second_payment || null;
+            data.datasheet = `./uploads/${datasheet[0].originalname}`;
+
+            const order = await ProviderOrder.create(data);
 
             return res.status(201).json({'data' : order});
         } catch (error) {
             return res.status(500).json({'error' : error.message});
         }
-    }
+    };
+
+    async list (req, res){
+        try {
+            const orders = await ProviderOrder.findAll();
+
+            return res.status(200).json({'data' : orders});
+        } catch (error) {
+            return res.status(500).json({'error' : error.message});
+        }
+    };
 }
 
 module.exports = new ProviderOrderController();
